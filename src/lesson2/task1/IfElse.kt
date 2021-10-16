67849db974f4746f04802df1f63616b65e5677b1
RD-MANGU <92602967+RD-MANGU@users.noreply.github.com>
2021-10-16 15:54:16 -0400

    First task solved

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,16 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String =
+    if (age / 10 % 10 == 1) {
+        "$age лет"
+    } else if (age % 10 == 1) {
+        "$age год"
+    } else if (age % 10 in 2..4) {
+        "$age года"
+    } else {
+        "$age лет"
+    }
 
 /**
  * Простая (2 балла)
