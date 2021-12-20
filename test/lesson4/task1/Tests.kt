3370a15a9a503a55011f193c220f5e2749633780
RD-MANGU <92602967+RD-MANGU@users.noreply.github.com>
2021-12-20 20:11:49 +0300

    Finished lesson

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -141,6 +141,7 @@ class Tests {
         assertEquals(13, polynom(listOf(3, 2), 5))
         assertEquals(0, polynom(listOf(2, -3, 1), 1))
         assertEquals(45, polynom(listOf(-7, 6, 4, -4, 1), -2))
+        assertEquals(14000, polynom(listOf(-1000, -1000), -15))
     }
 
     @Test
@@ -185,6 +186,7 @@ class Tests {
         assertEquals(listOf(1, 2, 1, 0), convert(100, 4))
         assertEquals(listOf(1, 3, 12), convert(250, 14))
         assertEquals(listOf(2, 14, 12), convert(1000, 19))
+        assertEquals(listOf(0), convert(0, 12))
     }
 
     @Test
