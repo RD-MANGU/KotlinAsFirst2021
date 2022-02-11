7b1f022b66eecdcf31116b38d7b16bf70e550b9c
RD-MANGU <92602967+RD-MANGU@users.noreply.github.com>
2022-02-11 17:01:28 -0400

    lesson9/task1: createMatrix, MatrixImpl completed

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -44,32 +44,58 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (listOf(height, width).any { it <= 0 }) throw IllegalArgumentException("Height or Width need to be more than 0.")
+    return MatrixImpl(height, width, e)
+}
 
 /**
  * Средняя сложность (считается двумя задачами в 3 балла каждая)
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
-
-    override val width: Int = TODO()
-
-    override fun get(row: Int, column: Int): E = TODO()
-
-    override fun get(cell: Cell): E = TODO()
-
+class MatrixImpl<E>(
+    override val height: Int, override val width: Int, e: E,
+    private val matrix: MutableList<E> = MutableList(width * height) { e }
+) : Matrix<E> {
+    override fun get(row: Int, column: Int): E = matrix[row + column * height]
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        matrix[row + column * height] = value
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun equals(other: Any?): Boolean = other is MatrixImpl<*> && other.matrix == this.matrix
+    //https://play.kotlinlang.org/byExample/03_special_classes/01_Data%20classes
+    //    override fun equals(other: Any?): Boolean {
+    //        if (this === other) return true
+    //        if (javaClass != other?.javaClass) return false
+    //
+    //        other as MatrixImpl<*>
+    //
+    //        if (height != other.height) return false
+    //        if (width != other.width) return false
+    //        if (matrix != other.matrix) return false
+    //
+    //        return true
+    //    }
+
+    override fun toString(): String {
+        val matrixStr = StringBuilder()
+        for (column in 0 until height) {
+            matrixStr.append(
+                matrix.subList(column * width, (column + 1) * width).joinToString(separator = "\t", postfix = "\n")
+            )
+        }
+        return matrixStr.toString()
     }
 
-    override fun equals(other: Any?) = TODO()
-
-    override fun toString(): String = TODO()
+    //KOTLIN SUGGESTED THE CODE BELOW
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + matrix.hashCode()
+        return result
+    }
 }
 
